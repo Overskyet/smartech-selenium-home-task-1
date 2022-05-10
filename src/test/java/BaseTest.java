83cdd72db0188b6bbf6e5aebafc3706bb1d00594
Overskyet <paul.overskyet@gmail.com>
2022-05-11 00:31:16 +0100

    BaseTest class added Sample test scenarios added

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public abstract class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        this.driver = new ChromeDriver();
+        new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,26 @@
+import org.testng.annotations.Test;
+import page.avtodispetcher.AvtodispetcherMainPage;
+import page.avtodispetcher.AvtodispetcherResultPage;
+import page.yandex.YandexResultPage;
+import page.yandex.YandexSearchPage;
+
+public class SampleTest extends BaseTest {
+
+    @Test
+    public void sampleFirstTestScenario() {
+        YandexSearchPage searchPage = new YandexSearchPage(driver);
+        searchPage.visit("https://yandex.ru/");
+
+        YandexResultPage resultPage = new YandexResultPage(driver);
+        resultPage.visit("https://yandex.ru/search/?lr=15&oprnd=9585134529&text=dfds");
+    }
+
+    @Test
+    public void sampleSecondTestScenario() {
+        AvtodispetcherMainPage mainPage = new AvtodispetcherMainPage(driver);
+        mainPage.visit("https://www.avtodispetcher.ru/distance/");
+
+        AvtodispetcherResultPage resultPage = new AvtodispetcherResultPage(driver);
+        resultPage.visit("https://www.avtodispetcher.ru/distance/?from=%D0%A2%D1%83%D0%BB%D0%B0&to=%D0%A1%D0%B0%D0%BD%D0%BA%D1%82-%D0%9F%D0%B5%D1%82%D0%B5%D1%80%D0%B1%D1%83%D1%80%D0%B3&v=&vt=car&rm=110&rp=90&rs=60&ru=40&fc=9&fp=42&ov=&atn=&aup=&atr=&afd=&ab=&acb=");
+    }
+}
