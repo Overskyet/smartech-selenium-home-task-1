070be3f100fbdd62c9366b23ddeea056b2cf54ad
Overskyet <paul.overskyet@gmail.com>
2023-01-07 18:45:16 +0000

    Logger has been added Refactoring

--- src/main/java/helper/driver/DriverListener.java
+++ src/main/java/helper/driver/DriverListener.java
@@ -1,5 +1,6 @@
 package helper.driver;
 
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
@@ -15,8 +16,7 @@ public class DriverListener implements WebDriverListener {
     //TODO Implement WebDriverListener
     // TODO Implement a Class that initializes log4j
     // TODO Implement a Class that takes an original WebDriver, prepares WebDriverListener and returns decorated WebDriver
-    private static final Logger logger = LogManager.getLogger(DriverListener.class);
-
+    Logger logger = LogManager.getLogger(DriverListener.class.getName());
     public WebDriver getDecoratedWebDriver(WebDriver original) {
         return new EventFiringDecorator<>(this).decorate(original);
     }
@@ -24,13 +24,15 @@ public class DriverListener implements WebDriverListener {
     @Override
     public void beforeAnyCall(Object target, Method method, Object[] args) {
 //        System.out.println(ZonedDateTime.now(ZoneId.of("UTC")) + ": " + "Before any call: " + target + "\n" + method + "\n" + Arrays.toString(args));
-        logger.debug("Before any call: " + target + "\n" + method + "\n" + Arrays.toString(args));
+        logger.log(Level.DEBUG, "Before any call: " + target + "\n" + method + "\n" + Arrays.toString(args));
+//        logger.debug("Before any call: " + target + "\n" + method + "\n" + Arrays.toString(args));
     }
 
     @Override
     public void afterAnyCall(Object target, Method method, Object[] args, Object result) {
 //        System.out.println(ZonedDateTime.now(ZoneId.of("UTC")) + ": " + "After any call: " + target + "\n" + method + "\n" + Arrays.toString(args) + "\n" + result);
-        logger.debug("After any call: " + target + "\n" + method + "\n" + Arrays.toString(args));
+        logger.log(Level.DEBUG, "After any call: " + target + "\n" + method + "\n" + Arrays.toString(args));
+//        logger.debug("After any call: " + target + "\n" + method + "\n" + Arrays.toString(args));
     }
 
 }
--- src/main/java/helper/logging/Logger.java
+++
@@ -1,6 +0,0 @@
-package helper.logging;
-
-public class Logger {
-    //TODO Init logger
-    // Public method to get initialized reporter
-}
