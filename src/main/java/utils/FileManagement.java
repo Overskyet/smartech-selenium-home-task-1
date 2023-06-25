844a62d34c9f870404a9a85278f0ee29ac6c3a99
Overskyet <paul.overskyet@gmail.com>
2023-06-25 19:54:33 +0100

    Unused method split() deleted

--- src/main/java/utils/FileManagement.java
+++ src/main/java/utils/FileManagement.java
@@ -13,17 +13,17 @@ import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 
 public class FileManagement {
-    /** List files under root project directory */
+    /** Lists files under root project directory */
     public static File[] listFilesWith(String extension) {
         return new File(Paths.get(".").normalize().toAbsolutePath().toString()).listFiles((dir, name) -> name.endsWith(extension));
     }
 
-    /** Save screenshot to the root project directory */
+    /** Saves screenshot to the root project directory */
     public static File saveScreenshotAs(String name, ImageExtension extension) throws IOException {
         return new File(generatePath(name + "_" + getCurrentUTCDateAndTimeWithSeconds(), extension));
     }
 
-    /** Save screenshot to the root project directory */
+    /** Saves screenshot to the root project directory */
     public static File saveScreenshot(String name) throws IOException {
         return new File(generatePath(name + "_" + getCurrentUTCDateAndTimeWithSeconds(), ImageExtension.PNG));
     }
@@ -35,20 +35,6 @@ public class FileManagement {
             Files.createDirectory(folderPath);
         }
         return folderPath.resolve(name + extension.toString()).toString();
-    }
-
-    private static String split(String path) {
-        String newPath = path;
-        int lastIndexWin = path.lastIndexOf('\\');
-        int lastIndexUnix = path.lastIndexOf('/');
-
-        if (lastIndexWin != -1) {
-            newPath = path.substring(0, lastIndexWin);
-        } else if (lastIndexUnix != -1) {
-            newPath = path.substring(0, lastIndexUnix);
-        }
-        System.out.println("Save screenshot to: " + newPath);
-        return newPath;
     }
 
     private static String getCurrentDate() {
