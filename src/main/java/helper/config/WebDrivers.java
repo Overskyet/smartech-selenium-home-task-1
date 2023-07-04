5630a28f40d0985698c40b3abe98014774990993
Overskyet <paul.overskyet@gmail.com>
2023-07-05 00:21:22 +0100

    Additional webdriver setup features have been added

--- src/main/java/helper/config/WebDrivers.java
+++ src/main/java/helper/config/WebDrivers.java
@@ -6,10 +6,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.time.Duration;
 import java.util.Properties;
 
 public class WebDrivers {
     private Properties driversSettings;
+    private final static String WEB_DRIVER_PROPERTY = "webdriver";
+    private final static String PAGE_LOAD_TIMEOUT_PROPERTY = "pageLoad.timeout";
 
     public WebDrivers() {
         setupDriversSettings();
@@ -19,7 +22,7 @@ public class WebDrivers {
     }
 
     public WebDriver initWebDriver() {
-        return getDriverFromSettings("webdriver");
+        return setupDriver();
     }
 
     private void setupDriversSettings() {
@@ -29,13 +32,14 @@ public class WebDrivers {
         driversSettings = new ConfigInitialization(fileName).setupProperties();
     }
 
-    private WebDriver getDriverFromSettings(String propertyKey) {
-        String driverToUse = getProperty(propertyKey);
+    private WebDriver setupDriver() {
+        String driverToUse = getProperty(WEB_DRIVER_PROPERTY);
+        String pageLoadTimeout = getProperty(PAGE_LOAD_TIMEOUT_PROPERTY);
 
         if (driverToUse == null || driverToUse.isBlank()) {
             throw new IllegalArgumentException(String.format("Can not initialize new WebDriver instance" +
                     "\nProperty key: '%s' does not exist" +
-                    "\nEnsure that the property key '%s' is in the properties file", propertyKey, propertyKey));
+                    "\nEnsure that the property key '%s' is in the properties file", WEB_DRIVER_PROPERTY, WEB_DRIVER_PROPERTY));
         }
 
         WebDriver driver;
@@ -54,8 +58,16 @@ public class WebDrivers {
                 driver = new EdgeDriver();
                 break;
             default: throw new IllegalArgumentException(String.format("Can not initialize new WebDriver instance, using provided parameter's value: %s" +
-                    "\nCheck the value of '%s' property key in the properties", driverToUse, propertyKey));
+                    "\nCheck the value of '%s' property key in the properties", driverToUse, WEB_DRIVER_PROPERTY));
         }
+
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+
+        if (!(pageLoadTimeout == null || pageLoadTimeout.isBlank()) && isNumeric(pageLoadTimeout)) {
+            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(Long.parseLong(pageLoadTimeout)));
+        }
+
         return driver;
     }
 
@@ -63,4 +75,12 @@ public class WebDrivers {
         return driversSettings.getProperty(propertyKey);
     }
 
+    private boolean isNumeric(String numString) {
+        try {
+            Long.parseLong(numString);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 }
