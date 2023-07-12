1d2ace4aa23dddebbbe83b94a1a22662e4a620f4
Overskyet <paul.overskyet@gmail.com>
2023-07-12 03:15:36 +0100

    snakeyaml dependency has been added

---
+++ src/main/java/com.overskyet/utils/ReadYAMLData.java
@@ -0,0 +1,11 @@
+package com.overskyet.utils;
+
+public class ReadYAMLData {
+    public static String[][] getDataFrom(String YAMLFilePath) {
+        return readDataFrom(YAMLFilePath);
+    }
+
+    private static String[][] readDataFrom(String YAMLFilePath) {
+        return null;
+    }
+}
